d2c1cdf4f4fadda308fcb2c65317d2e80ce50dc3
EmileSpecialProducts <130773873+EmileSpecialProducts@users.noreply.github.com>
2025-07-09 11:52:07 +0200

    Updated the Path to the editor.css file

--- ESP-SD-Async-Web-Server/editor.js
+++ ESP-SD-Async-Web-Server/editor.js
@@ -2,7 +2,7 @@
 
 var link = document.createElement("link");
 link.rel = "stylesheet";
-link.href = "https://emilespecialproducts.github.io/ESP-LittleFS-Async-Web-Server/editor.css";
+link.href = "https://emilespecialproducts.github.io/ESP-SD-Async-Web-Server/editor.css";
 
 document.head.appendChild(link);
 
